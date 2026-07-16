5b96a852add952f1e14c768223992e47613ef9ec
i <70916412+KrispWaffle@users.noreply.github.com>
2026-07-15 19:28:03 -0700

    Add: max-water-container - submission-1

---
+++ Data Structures & Algorithms/max-water-container/submission-1.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int left{0};
+        int right{static_cast<int>(height.size() - 1)};
+        int max{0};
+        while (left < right) {
+            int water = std::abs((right - left) *
+                                 std::min(height[left], height[right]));
+            max= std::max(max,water);
+            if (height[left] > height[right]) {
+                right--;
+            } else {
+                left++;
+            }
+        }
+        return max;
+    }
+};
